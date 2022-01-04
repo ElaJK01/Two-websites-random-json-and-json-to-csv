d711771d656a6b443fd78ad71b851aa878c8a4c6
Elżbieta Janus-Kaleta <60756867+ElaJK01@users.noreply.github.com>
2022-01-04 22:31:03 +0100

    random json website after eslint

--- Website_random_json/src/app.js
+++ Website_random_json/src/app.js
@@ -1,15 +1,15 @@
-const express = require('express')
+const express = require('express');
 const helpers = require('../helpers');
-const app = express()
-const port = 3000
 
+const app = express();
+const port = 3000;
 
 app.get('/generate/json/:size', (req, res) => {
-  const list_size = req.params.size
-  const dataJson = helpers.createJsonList(list_size)
-  res.send(dataJson)
-})
+  const listSize = req.params.size;
+  const dataJson = helpers.createJsonList(listSize);
+  res.send(dataJson);
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
